e7d20eab29d43ac555480102bff251daad8556d6
Andy Khau <fakeemail1441@gmail.com>
2025-06-11 03:21:17 -0700

    Improve thumnbnail clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export const metadata = {
         description: "Check my stuff out.",
         images: [
             {
-                url: "/thumbnail.png",
+                url: "/thumbnail.jpg",
                 width: 1200,
                 height: 630,
                 alt: "Andy Khau",
@@ -24,7 +24,7 @@ export const metadata = {
         card: "summary_large_image",
         title: "Andy Khau",
         description: "Check my stuff out.",
-        images: ["/thumbnail.png"],
+        images: ["/thumbnail.jpg"],
     },
 };
 
