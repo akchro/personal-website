c56b1120d065aa3de3642b93e41b34030320c00b
Andy Khau <fakeemail1441@gmail.com>
2025-06-11 03:40:21 -0700

    Attempt to force imessage to see new thumbnail
    Add hard meta tags
    
    Fix thumbnail typos
    
    Testing removing links
    
    Readd link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Head from "next/head";
 import ProjectCard from "@/components/ProjectCard";
 import Footer from "@/components/Footer";
 import ThreeBackground from "@/components/ThreeBackground";
@@ -13,7 +14,7 @@ export const metadata = {
         description: "Check my stuff out.",
         images: [
             {
-                url: "/thumbnail.jpg",
+                url: "/thumbnail_v2.jpg",
                 width: 1200,
                 height: 630,
                 alt: "Andy Khau",
@@ -24,14 +25,22 @@ export const metadata = {
         card: "summary_large_image",
         title: "Andy Khau",
         description: "Check my stuff out.",
-        images: ["/thumbnail.jpg"],
+        images: ["/thumbnail_v2.jpg"],
     },
 };
 
 
 export default function Home() {
     return (
+
         <main className={"min-h-screen"}>
+            <Head>
+                <meta property="og:title" content="Andy Khau" />
+                <meta property="og:description" content="Check my stuff out." />
+                <meta property="og:image" content="https://andykhau.com/thumbnail_v2.jpg" />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://andykhau.com/" />
+            </Head>
             <ThreeBackground />
             <div className={"h-full flex justify-center items-center h-screen"}>
                 <div className={'flex flex-col'}>
@@ -47,7 +56,7 @@ export default function Home() {
             <div className={'mx-20 mb-10 min-h-screen'} id={'projects'}>
                 <h2 className={'text-4xl text-center mb-20'}>Projects</h2>
                 <div className={'flex gap-10 flex-wrap justify-center'}>
-                    <ProjectCard title={'Eyesite'} image={'eyesite.jpg'} link={'https://eyesite.andykhau.com'} />
+                    <ProjectCard title={'Eyesite'} image={'eyesite.jpg'} link={'https://eyesite.andykhau.com/'} />
                     <ProjectCard title={'Waddling Life'} image={'waddling.jpg'} link={'https://waddlinglife.com/'}/>
                     <ProjectCard title={"Google Sheets MCP"} image={'sheetsmcp.jpg'} link={'https://github.com/akchro/google-sheets-mcp'} />
                     <ProjectCard title={'Accountable'} image={'accountable.jpg'} link={'https://devpost.com/software/accountable-tnij3l'}/>
